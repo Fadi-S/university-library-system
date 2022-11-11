13b8b365608c5997078d05e824edc7648901f00e
Fadi Sarwat <fadisarwat@yahoo.com>
2022-11-11 20:02:17 +0200

    Changes

---
+++ src/frontend/librarian/ViewBorrowedBooks.java
@@ -0,0 +1,51 @@
+package frontend.librarian;
+
+import backend.library.users.LibrarianRole;
+import backend.library.utils.StudentBook;
+import frontend.Page;
+
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicTableUI;
+import java.time.format.DateTimeFormatter;
+
+public class ViewBorrowedBooks implements Page {
+    private JFrame frame;
+    private JPanel panel;
+    private JTable table;
+
+    public ViewBorrowedBooks() {
+        frame = new JFrame("View Borrowed Books");
+        frame.setContentPane(panel);
+
+        backend.library.users.LibrarianRole role = new LibrarianRole();
+
+        StudentBook[] studentBooks = role.getListOfBorrowingOperations();
+        String[][] data = new String[studentBooks.length][3];
+
+        for (int i=0; i <studentBooks.length; i++) {
+            data[i][0] = studentBooks[i].getStudentId();
+            data[i][1] = studentBooks[i].getBookId();
+            data[i][2] = studentBooks[i].getBorrowDate().format(DateTimeFormatter.ISO_DATE);
+        }
+
+        String[] columns = {
+            "Student ID",
+            "Book ID",
+            "Borrow Date"
+        };
+        table = new JTable(data, columns) {
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        table.setUI(new BasicTableUI());
+        table.setBounds(30, 40, 200, 300);
+
+        frame.add(new JScrollPane(table));
+    }
+
+    @Override
+    public JFrame getFrame() {
+        return frame;
+    }
+}
